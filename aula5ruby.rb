6a6781d665c79a00f3be5e2a366110fbef18e40c
JoaoDetore7 <joaodetore7@gmail.com>
2023-07-14 20:11:01 -0300

    aula5: exemplos

---
+++ aula5ruby.rb
@@ -0,0 +1,25 @@
+#exemplos classes
+    # class Pessoa
+    # attr_accessor :nome, :idade
+
+    #     def initialize(nome,idade)
+    #         @nome = nome
+    #         @idade = idade
+    #     end
+
+    #     def saudacao
+    #         puts "Olá meu nome é #{nome} e tenho #{idade} anos."
+    #     end
+
+    # end
+
+    # pessoa1 = Pessoa.new("João", 21)
+    # pessoa2 = Pessoa.new("Giovani", 22)
+
+    # puts pessoa1.nome
+    # puts pessoa2.idade
+    # pessoa1.saudacao
+    # pessoa2.saudacao
+
+
+    
