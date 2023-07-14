239006492d82681febab7bece87fad78e100d96a
JoaoDetore7 <joaodetore7@gmail.com>
2023-07-14 20:11:18 -0300

    ex1: resolução

---
+++ ex1.rb
@@ -0,0 +1,25 @@
+class Pessoa
+
+        def nome
+        @nome
+        end
+
+        def nome=(novo_nome)
+        @nome = novo_nome
+        end
+
+        def idade
+        @idade
+        end
+
+        def idade=(nova_idade)
+        @idade = nova_idade
+        end
+end
+
+pessoa1 = Pessoa.new
+pessoa1.nome = "João"
+pessoa1.idade = 21
+
+puts pessoa1.nome
+puts pessoa1.idade
